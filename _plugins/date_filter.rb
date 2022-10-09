0f128b77427f2a57c992b1579e14dd70f044cd64
Floris Huetink <florishuetink@gmail.com>
2022-10-09 21:54:09 +0200

    Simplify homepage

---
+++ _plugins/date_filter.rb
@@ -0,0 +1,9 @@
+module DateFilter
+  MONTHS = %w(januari februari maart april mei juni juli augustus september oktober november december)
+
+  def nl_long_date(input)
+    input.strftime("%d") + " " + MONTHS[input.strftime("%m").to_i - 1] + " " + input.strftime("%Y")
+  end
+end
+
+Liquid::Template.register_filter(DateFilter)
