928f157333257d6e852d693dc082dd4c59f5144c
Floris Huetink <florishuetink@gmail.com>
2022-11-16 21:25:43 +0100

    Remove zero-padding from day of month

--- _plugins/date_filter.rb
+++ _plugins/date_filter.rb
@@ -2,7 +2,7 @@ module DateFilter
   MONTHS = %w(januari februari maart april mei juni juli augustus september oktober november december)
 
   def nl_long_date(input)
-    input.strftime("%d") + " " + MONTHS[input.strftime("%m").to_i - 1] + " " + input.strftime("%Y")
+    input.strftime("%-d") + " " + MONTHS[input.strftime("%m").to_i - 1] + " " + input.strftime("%Y")
   end
 end
 
